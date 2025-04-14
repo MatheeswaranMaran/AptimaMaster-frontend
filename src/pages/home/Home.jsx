ebb5525e577e8c62de696d31b646a7615058d261
Matheeswaran M <matheeswaranm007@gmail.com>
2025-04-14 16:45:48 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import './home.css'
+import './Home.css'
 import Testimonials from '../../components/testimonials/Testimonials';
 
 const Home = () => {
