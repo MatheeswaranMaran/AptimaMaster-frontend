a2923d7f190b5c2c8d442d1157bbcc476cfc09f0
Matheeswaran M <matheeswaranm007@gmail.com>
2025-04-14 16:50:34 +0530

    Update AdminCourses.jsx

--- src/admin/Courses/AdminCourses.jsx
+++ src/admin/Courses/AdminCourses.jsx
@@ -3,7 +3,7 @@ import Layout from "../Utils/Layout";
 import { useNavigate } from "react-router-dom";
 import { CourseData } from "../../context/CourseContext";
 import CourseCard from "../../components/coursecard/CourseCard";
-import "./admincourses.css";
+import "./AdminCourses.css";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { server } from "../../main";
